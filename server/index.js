ea647d3d973ee71ac053571a64362044d1caec28
Salmanabdullah <salmandcc@gmail.com>
2023-01-23 23:38:35 +0100

    dotenv added

--- server/index.js
+++ server/index.js
@@ -3,21 +3,18 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const dotenv= require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
-const CONNECTION_URL = 'mongodb+srv://salman2023:salman9900@cluster0.trl8iyf.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
-
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() => console.log("connected"))
   .catch(() => console.log("No connection"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
 });
