4440339dfe19a07033e30cbb7ac77a43a6d4ae5c
Salmanabdullah <salmandcc@gmail.com>
2023-03-29 00:31:49 +0600

    model

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,9 @@
+import PostMessage from "../models/postMessage";
+
 export const getPosts = (req, res) => {
   res.send("It works!");
 };
+
+export const createPost = (req, res) =>{
+  res.send("Post creation")
+}
